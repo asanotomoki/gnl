2d15ef8ae146dd34b72a89ee3752cdd1899fedd3
智貴 <test@asanosatoshitakanoMacBook-Air.local>
2022-06-07 15:06:52 +0900

    6/7 tester

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,29 @@
 /*   By: test <test@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 00:32:44 by test              #+#    #+#             */
-/*   Updated: 2022/06/06 22:27:24 by test             ###   ########.fr       */
+/*   Updated: 2022/06/07 15:03:48 by test             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "get_next_line.h"
+static char *ft_newstrage(char *strage);
+static char *ft_read_buf(int fd, char *strage);
+
 
 char *get_next_line(int fd) {
 	static char *strage;
 	char *res;
-	//bffersizeのチェック
-	//static領域に改行があるかを確認
 	if(BUFFER_SIZE <= 0) 
 		return (NULL);
-	if (ft_strchr(strage, '\n')) {
-		res = ft_format(strage);
+	if (!ft_strchr(strage, '\n')) {
+		strage = ft_read_buf(fd, strage);
+		if (strage == NULL)
+			return NULL;
+	}
+	res = ft_format(strage);
+	if (res != NULL)
 		strage = ft_newstrage(strage);
-		return (res);
-	}
-	else {
-		strage = ft_strchr(strage, '\n') + 1;
-	}
-	return (strage);
+	return (res);
 }
 
 char *ft_format(char *strage) {
@@ -39,19 +40,54 @@ char *ft_format(char *strage) {
 	len = 0;
 	while (ft_strchr(ch, strage[len]) == NULL)
 		len++;
-	if (len == 0)
+	if (len == 0 && strage[0] != '\n')
 		return (NULL);
-	len++;
-	res = (char *)malloc(sizeof(char) * (len + 1));
-	if (res == NULL) {
-		free(strage);
-		return (0);
-	}
-	ft_strlcpy(res, strage, len + 1);
+	len = len + 1 + (strage[len] == '\n');
+	res = (char *)malloc(sizeof(char) * len);
+	if (res == NULL) 
+		return (NULL);
+	ft_strlcpy(res, strage, len);
 	return (res);
 }
 
-static char ft_newstrage(char strage)
-{
+static char *ft_read_buf(int fd, char *strage) {
+	char *buf;
+	size_t read_size;
 	
+	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (buf == NULL)
+		return (NULL);
+	read_size = read(fd, buf, (size_t)BUFFER_SIZE);
+	if (read_size < 0)
+	{
+		free(buf);
+			if (read_size == 0)
+				return (strage);
+		return (NULL);
+	}
+	buf[read_size] = '\0';
+	strage = ft_newstrage(buf);
+	return (strage);
+}
+
+static char *ft_newstrage(char *strage)
+{
+	char *newstrage;
+	char *ch;
+	size_t len;
+
+	if (strage == NULL)
+		return (NULL);
+	if (strage[0] == '\0')
+	{
+		free(strage);
+		return (NULL);
+	}
+	ch =  "\n";
+	len = 0;
+	while (ft_strchr(ch, strage[len]) == NULL)
+		len++;
+	newstrage = ft_strdup(strage + len);
+	free(strage);
+	return (newstrage);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -7,3 +7,4 @@ size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char *ft_format(char *strage);
+char	*ft_strdup(const char *s1);
