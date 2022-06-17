f483d07cb32326c2a1a7a9bfc6117ad66eda173a
智貴 <test@asanosatoshitakanoMacBook-Air.local>
2022-06-17 11:30:50 +0900

    bonus header

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,11 +6,11 @@
 /*   By: test <test@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 00:32:48 by test              #+#    #+#             */
-/*   Updated: 2022/06/08 16:35:03 by test             ###   ########.fr       */
+/*   Updated: 2022/06/14 22:21:54 by test             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 size_t	ft_strlen(const char *s)
 {
